c7fae80e9dcc6934dd476357aa8f37b05e5abe25
victor.luna <victor.luna@hibrido.com.br>
2023-12-14 00:47:15 -0300

    cetaphil

--- scripts/cetaphil-lp-sf.js
+++ scripts/cetaphil-lp-sf.js
@@ -1,48 +1,94 @@
-// window.addEventListener("DOMContentLoaded", () => {
-//   const contactForm = document.querySelector("");
+window.addEventListener("DOMContentLoaded", () => {
+  const contactForm = document.getElementById("contactForm");
+  const nomeInput = document.getElementById("nome");
+  const sobrenomeInput = document.getElementById("sobrenome");
+  const dataNascimentoInput = document.getElementById("dataNascimento");
+  const emailInput = document.getElementById("email");
+  const linkVideoInput = document.getElementById("linkVideo");
+  const telefoneInput = document.getElementById("telefone");
+  const termosCheckbox1Input = document.getElementById("termosCheckbox1");
+  const termosCheckbox2Input = document.getElementById("termosCheckbox2");
+  const newsletterCheckboxInput = document.getElementById("newsletterCheckbox");
 
-//   const maskPhoneInput = () => {
-//     IMask(document.getElementById("phone"), {
-//       mask: "(00) 00000-0000"
-//     });
-//   };
+  const maskPhoneInput = () => {
+    IMask(telefoneInput, {
+      mask: "(00) 00000-0000"
+    });
+  };
 
-//   maskPhoneInput();
+  maskPhoneInput();
 
-//   contactForm.addEventListener("submit", (e) => {
-//     const data = {
-//       nome,
-//       sobrenome,
-//       dataNascimento,
-//       email,
-//       linkVideo,
-//       Telefone: "55" + Telefone.replace(/\D/g, ""),
-//       optinEmail: Optin ? "true" : "false"
-//     };
+  contactForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-//     let formData = new FormData();
+    const nome = nomeInput.value;
+    const sobrenome = sobrenomeInput.value;
+    const dataNascimento = dataNascimentoInput.value;
+    const email = emailInput.value;
+    const linkVideo = linkVideoInput.value;
+    const telefone = telefoneInput.value;
+    const termosCheckbox1 = termosCheckbox1Input.checked;
+    const termosCheckbox2 = termosCheckbox2Input.checked;
+    const newsletterCheckbox = newsletterCheckboxInput.checked;
 
-//     Object.entries(data).forEach((position) => {
-//       const key = position[0];
-//       const value = position[1];
-//       formData.append(key, value);
-//     });
+    if (
+      !nome ||
+      !sobrenome ||
+      !dataNascimento ||
+      !email ||
+      !linkVideo ||
+      !telefone ||
+      !termosCheckbox1 ||
+      !termosCheckbox2
+    ) {
+      return;
+    }
 
-//     fetch("/integracao-cetaphl-sf", { method: "POST", body: formData })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         if (data.statusCode == 400) {
-//           formButton.innerText = "Enviar";
-//           Swal.fire("Ops!", "E-mail já cadastrado!", "error");
-//         } else if (data.statusCode == 500) {
-//           formButton.innerText = "Enviando";
-//           Swal.fire("Ops!", "Revise seus dados e tente novamente!", "error");
-//         } else {
-//           // POST REALIZADO COM SUCESSI
-//         }
-//       })
-//       .finally(() => {
-//         //
-//       });
-//   });
-// });
+    const data = {
+      nome,
+      sobrenome,
+      dataNascimento,
+      email,
+      linkVideo,
+      Telefone: "55" + telefone.replace(/\D/g, ""),
+      optinEmail: newsletterCheckbox ? "true" : "false"
+    };
+
+    console.log(data);
+
+    let formData = new FormData();
+    formData.append("nome", nome);
+    formData.append("sobrenome", sobrenome);
+    formData.append("dataNascimento", dataNascimento);
+    formData.append("email", email);
+    formData.append("linkVideo", linkVideo);
+    formData.append("Telefone", "55" + telefone.replace(/\D/g, ""));
+    formData.append("optinEmail", newsletterCheckbox ? "true" : "false");
+    console.log(formData);
+
+    try {
+      const response = await fetch("/integracao-cetaphil-sf", {
+        method: "POST",
+        body: formData
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro no envio dos dados");
+      }
+
+      const responseData = await response.json();
+
+      if (responseData.statusCode === 400) {
+        Swal.fire("Ops!", "E-mail já cadastrado!", "error");
+      } else if (responseData.statusCode === 500) {
+        Swal.fire("Ops!", "Revise seus dados e tente novamente!", "error");
+      } else {
+        Swal.fire("Sucesso!", "Cadastro realizado com sucesso!", "success");
+      }
+    } catch (error) {
+      console.error(error);
+      console.log(formData);
+      Swal.fire("Ops!", "Ocorreu um erro. Tente novamente mais tarde!", "error");
+    }
+  });
+});
